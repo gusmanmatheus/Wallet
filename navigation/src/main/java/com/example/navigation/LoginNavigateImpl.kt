000078244a9da0e58f28b47aba73dd27095de612
Matheus Gusman <matheusenrik@hotmail.com>
2020-08-14 19:00:30 -0300

    criando modulo de navegacao

---
+++ navigation/src/main/java/com/example/navigation/LoginNavigateImpl.kt
@@ -0,0 +1,18 @@
+package com.example.navigation
+
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.navigation.utils.navigate
+import com.mathe.login.navigation.LoginNavigate
+
+class LoginNavigateImpl(private val fragment: Fragment): LoginNavigate {
+    override fun actionLogin() {
+        TODO("Not yet implemented")
+      }
+
+    override fun actionRegister() {
+        TODO("Not yet implemented")
+    }
+
+}
+
